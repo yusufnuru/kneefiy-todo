c229a3a1d2db03beaddfabde2922a1c9bfd15511
yusufnuru <yusufnuru2002@gmail.com>
2025-07-07 16:19:37 +0800

    fix: update environment variable config

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,5 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 
 // This check can be removed, it is just for tutorial purposes
 export const hasEnvVars =
-  process.env.SUPABASE_URL &&
-  process.env.SUPABASE_ANON_KEY;
+  process.env.NEXT_PUBLIC_SUPABASE_URL &&
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;

cc41ae9a5dd3bcc9c3016bbd7ffee6910c9e0b58
yusufnuru <yusufnuru2002@gmail.com>
2025-07-11 12:08:02 +0800

    feat: update Home page layout and content for improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
@@ -7,6 +6,7 @@ import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-ste
 import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -15,17 +15,27 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Keenify Todo App</Link>
+              <Link href={"/"}>Keenify Todo</Link>
               <ThemeSwitcher />
             </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton pathname={"/"} />}
+            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton/>}
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+            <h2 className="font-medium text-xl mb-4">Keenify Todo App</h2>
+            <div className="prose dark:prose-invert">
+              <p>Welcome to Keenify Todo, your personal task management solution.</p>
+            {hasEnvVars ? (
+              <div className="mt-6">
+            
+                  <Button variant="outline"><Link href="/todo">Go to Todo</Link></Button>
+              
+              </div>
+            ) : (
+              <ConnectSupabaseSteps />
+            )}
+          </div>
           </main>
         </div>
 
